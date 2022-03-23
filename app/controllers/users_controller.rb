71c7e7fb870cb48cd9e0d35266dd3374dfc11167
Eliot <eliotpitman@gmail.com>
2022-03-23 00:45:57 -0500

    init

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user, except: :create
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if @user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+  def show
+
+    @user = User.find_by(email: params[:email])
+
+    render json: @user.as_json
+    
+  end
+end
